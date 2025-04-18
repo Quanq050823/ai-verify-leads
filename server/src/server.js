f2cae1f137026913af2446c8591a0013389ec019
Tran Quang <21110793@student.hcmute.edu.vn>
2025-04-18 07:51:50 +0700

    feat(auth): integrate login, logout, and register APIs with frontend

--- server/src/server.js
+++ server/src/server.js
@@ -14,48 +14,48 @@ import passport from "passport";
 import Producer from "./config/rabbitMQ.js";
 
 const START_SERVER = () => {
-    process.env["NODE_TLS_REJECT_UNAUTHORIZED"] = 0;
+	process.env["NODE_TLS_REJECT_UNAUTHORIZED"] = 0;
 
-    const app = express();
+	const app = express();
 
-    app.use(cookieParser());
-    // Configure express-session middleware
-    app.use(
-        session({
-            secret: "your-secret-key", // Replace with a strong secret key
-            resave: false,
-            saveUninitialized: true,
-            cookie: { secure: false }, // Set to true if using HTTPS
-        })
-    );
-    app.use(passport.initialize());
-    app.use(express.json({ extended: true }));
-    // app.use(
-    //     cors({
-    //         origin: `${config.feUrl}`,
-    //         credentials: true,
-    //     })
-    // );
-    app.use(express.urlencoded({ extended: true }));
-    app.use(morgan("combined"));
-    const port = config.port;
+	app.use(cookieParser());
+	// Configure express-session middleware
+	app.use(
+		session({
+			secret: "your-secret-key", // Replace with a strong secret key
+			resave: false,
+			saveUninitialized: true,
+			cookie: { secure: false }, // Set to true if using HTTPS
+		})
+	);
+	app.use(passport.initialize());
+	app.use(express.json({ extended: true }));
+	app.use(
+		cors({
+			origin: `${config.feUrl}`,
+			credentials: true,
+		})
+	);
+	app.use(express.urlencoded({ extended: true }));
+	app.use(morgan("combined"));
+	const port = config.port;
 
-    routes(app);
+	routes(app);
 
-    app.listen(config.port, () => {
-        console.log(chalk.blueBright(`Listen on http://localhost:${port}`));
-    });
+	app.listen(config.port, () => {
+		console.log(chalk.blueBright(`Listen on http://localhost:${port}`));
+	});
 };
 
 db()
-    .then(() => {})
-    .then(async () => {
-        await Producer.createChannel();
-    })
-    .then(() => {
-        START_SERVER();
-    })
-    .catch((error) => {
-        console.error(chalk.red("Error on starting server: ", error));
-        process.exit(0);
-    });
+	.then(() => {})
+	.then(async () => {
+		await Producer.createChannel();
+	})
+	.then(() => {
+		START_SERVER();
+	})
+	.catch((error) => {
+		console.error(chalk.red("Error on starting server: ", error));
+		process.exit(0);
+	});

9bffafda4c19e89525d66e0a36cd03ce8e6cbe42
Tran Quang <21110793@student.hcmute.edu.vn>
2025-04-27 17:05:44 +0700

    be/fix(api): add param pages_manage_ads to FB route

--- server/src/routes/facebookRoute.js
+++ server/src/routes/facebookRoute.js
@@ -11,31 +11,36 @@ import("../middlewares/facebookAuthMiddleware.js");
 const router = express.Router();
 
 router.get("/connect/:userId", (req, res, next) => {
-    passport.authenticate("facebook", {
-        scope: [
-            "email",
-            "public_profile",
-            "ads_management",
-            "pages_show_list",
-            "leads_retrieval",
-            "pages_read_engagement",
-            "pages_manage_metadata",
-        ],
-        state: req?.params?.userId,
-    })(req, res, next);
+	passport.authenticate("facebook", {
+		scope: [
+			"email",
+			"public_profile",
+			"ads_management",
+			"pages_show_list",
+			"leads_retrieval",
+			"pages_read_engagement",
+			"pages_manage_metadata",
+			"pages_manage_ads",
+		],
+		state: req?.params?.userId,
+	})(req, res, next);
 });
 router.get(
-    "/callback",
-    passport.authenticate("facebook", {
-        failureRedirect: "/auth/api/login",
-        failureMessage: true,
-        session: false,
-    }),
-    controller.connectFacebook
+	"/callback",
+	passport.authenticate("facebook", {
+		failureRedirect: "/auth/api/login",
+		failureMessage: true,
+		session: false,
+	}),
+	controller.connectFacebook
 );
 router.get("/pages/:profileId", authenticate, controller.getPages);
 router.get("/forms/:pageId", authenticate, controller.getForm);
 router.post("/subscribePage/:pageId", authenticate, controller.subscribePage);
-router.delete("/unsubscribePage/:pageId", authenticate, controller.unsubscribePage);
+router.delete(
+	"/unsubscribePage/:pageId",
+	authenticate,
+	controller.unsubscribePage
+);
 
 export default router;

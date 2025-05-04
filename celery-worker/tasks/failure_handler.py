c8d0e6911705a2e322d051714f308fb6c85f1034
Moderator <akirajin149@gmail.com>
2025-05-04 23:04:51 +0700

    worker/feat (failure handler): add failure handler class, update status and error

---
+++ celery-worker/tasks/failure_handler.py
@@ -0,0 +1,32 @@
+from celery import Task
+
+from utils.dbUtils import *
+import traceback
+
+class FailureHandler(Task):
+    def on_failure(self, exc, task_id, args, kwargs, einfo):
+        # Log the error or perform any other failure handling logic
+        print(f"Task {self.name} failed: {exc}")
+        # print(f"Task ID: {task_id}")
+        # print(f"Arguments: {args}")
+        # print(f"Keyword Arguments: {kwargs}")
+        tb_string = ''.join(traceback.format_exception(
+            type(exc), 
+            exc, 
+            exc.__traceback__
+        ))
+
+        print("")
+        
+        data = kwargs["message"]
+        update_field = {
+            "status": 0,
+            "error": {
+                "status": True,
+                "message": (f"Task {self.name} failed: {exc}"),
+                "taskId": task_id,
+                "stackTrace": tb_string[:5000]
+            },
+        }
+        
+        update_lead(data["leadId"], update_field)

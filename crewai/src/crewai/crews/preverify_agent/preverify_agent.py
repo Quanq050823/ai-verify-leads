42aa5ee7db3ef7e78079293195808c12fa8d6ae0
Tran Quang <21110793@student.hcmute.edu.vn>
2025-05-17 18:43:31 +0700

    crewai/fix: Optimize pre-verify agent

--- crewai/src/crewai/crews/preverify_agent/preverify_agent.py
+++ crewai/src/crewai/crews/preverify_agent/preverify_agent.py
@@ -3,19 +3,11 @@ from crewai.project import CrewBase, agent, crew, task
 from crewai_tools import FileWriterTool
 
 
-
-file_writer_tool_lead_raw_data = FileWriterTool(
-    filename="lead_raw_data_result.txt",
-    directory="preverify_agent",
-)
-
 file_writer_tool_preverify_lead = FileWriterTool(
-    filename="preverify_lead_result.txt",
+    filename="result.txt",
     directory="preverify_agent",
 )
-# If you want to run a snippet of code before or after the crew starts,
-# you can use the @before_kickoff and @after_kickoff decorators
-# https://docs.crewai.com/concepts/crews#example-crew-class-with-decorators
+
 
 
 @CrewBase
@@ -23,25 +15,11 @@ class PreverifyAgent:
 
     agents_config = "config/agents.yaml"
     tasks_config = "config/tasks.yaml"
-
-    @agent
-    def read_lead_data_agent(self) -> Agent:
-        return Agent(
-            config=self.agents_config["read_lead_data_agent"],  # type: ignore[index]
-            tools=[file_writer_tool_lead_raw_data],
-        )
-    
     @agent
     def preverify_lead_agent(self) -> Agent:
         return Agent(
             config=self.agents_config["preverify_lead_agent"],  # type: ignore[index]
             tools=[file_writer_tool_preverify_lead],
-        )
-
-    @task
-    def read_lead(self) -> Task:
-        return Task(
-            config=self.tasks_config["read_lead"],  # type: ignore[index]
         )
 
     @task
